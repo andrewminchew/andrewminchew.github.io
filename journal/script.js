b51625e2b6d810c3e1aee78ff2a8c944ffbad62c
andrewminchew <andrewminchew@gmail.com>
2023-04-14 07:42:59 -0500

    items now have a unique data-id based on timestamp, but items still sort by timestamp value
    additionally, delete, hide and edit work by addressing the item by data-id

--- journal/script.js
+++ journal/script.js
@@ -75,8 +75,9 @@ function submitForm() {
   const inputValue = inputField.value;
   if (inputValue) {
     const timestamp = new Date().toISOString();
+    const id = timestamp.replace(/[^\w]/g, ''); // remove all non-alphanumeric characters
     let itemsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
-    itemsArray.push({ text: inputValue, timestamp, visibility: "show" });
+    itemsArray.push({ id, text: inputValue, timestamp, visibility: "show" });
     localStorage.setItem('items', JSON.stringify(itemsArray));
     inputField.value = '';
     renderItems();
@@ -86,23 +87,23 @@ function submitForm() {
 outputContainer.addEventListener('click', (event) => {
   // Use event delegation to listen for clicks on the delete button
   if (event.target.id === 'delete-entry') {
-    // Get the timestamp of the parent element of the clicked delete button
-    const timestamp = event.target.closest('.entry').querySelector('.timestamp').textContent;
+    // Get the id of the parent element of the clicked delete button
+    const id = event.target.closest('.entry').dataset.id;
     let itemsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
-    // Remove the entry with the matching timestamp from the items array
-    itemsArray = itemsArray.filter(item => item.timestamp !== timestamp);
+    // Remove the entry with the matching id from the items array
+    itemsArray = itemsArray.filter(item => item.id !== id);
     localStorage.setItem('items', JSON.stringify(itemsArray));
     renderItems(); // Refresh the view
   }
 
   // Use event delegation to listen for clicks on the hide button
   if (event.target.id === 'hide-entry') {
-    // Get the timestamp of the parent element of the clicked hide button
-    const timestamp = event.target.closest('.entry').querySelector('.timestamp').textContent;
+    // Get the id of the parent element of the clicked hide button
+    const id = event.target.closest('.entry').dataset.id;
     let itemsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
-    // Set the visibility of the entry with the matching timestamp to "hide"
+    // Set the visibility of the entry with the matching id to "hide"
     itemsArray.forEach(function (item) {
-      if (item.timestamp === timestamp) {
+      if (item.id === id) {
         item.visibility = "hide";
       }
     });
@@ -111,12 +112,13 @@ outputContainer.addEventListener('click', (event) => {
   }
 
   if (event.target.id === 'change-date') {
-    // Get the timestamp of the parent element of the clicked date change button
-    const timestamp = event.target.closest('.entry').querySelector('.timestamp').textContent;
+    // Get the id of the parent element of the clicked date change button
+    const id = event.target.closest('.entry').dataset.id;
     // Show the date picker modal
     const modal = document.getElementById('date-picker-modal');
     modal.style.display = "block";
     const newDatePicker = document.getElementById('new-date-picker');
+    const timestamp = event.target.closest('.entry').querySelector('.timestamp').textContent;
     newDatePicker.value = timestamp.split('T')[0]; // Set the initial value of the date picker to the current timestamp
     // Add an event listener to the "Save" button
     const saveBtn = document.getElementById('save-date-btn');
@@ -124,9 +126,9 @@ outputContainer.addEventListener('click', (event) => {
       const newDate = new Date(newDatePicker.valueAsNumber);
       const newTimestamp = newDate.toISOString();
       let itemsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
-      // Update the timestamp of the entry with the matching timestamp
+      // Update the timestamp of the entry with the matching id
       itemsArray.forEach(function (item) {
-        if (item.timestamp === timestamp) {
+        if (item.id === id) {
           item.timestamp = newTimestamp;
         }
       });
@@ -137,17 +139,20 @@ outputContainer.addEventListener('click', (event) => {
   }
 });
 
+
 function renderItems() {
   let itemsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
   outputContainer.innerHTML = '';
   if (itemsArray.length) {
+    itemsArray.sort((a, b) => new Date(a.timestamp) - new Date(b.timestamp));
     itemsArray.forEach(function (item) {
       if (item.visibility !== "hide") {
         const itemElement = document.createElement('div');
         itemElement.classList.add('entry');
         itemElement.setAttribute('tabindex', '-1');
-        itemElement.innerHTML = `<div class="text"><span class="timestamp">${item.timestamp}</span>${item.text}</div><div class="toolbar"><button id="change-date">change&nbsp;date</button><button id="delete-entry">delete</button><button id="hide-entry">hide</button>
-        </div>`;
+        itemElement.setAttribute('data-id', `${item.id}`);
+        const timestamp = new Date(item.timestamp).toLocaleString(undefined, {timeZoneName: 'short'});
+        itemElement.innerHTML = `<div class="text"><span class="timestamp">${timestamp}</span>${item.text}</div><div class="toolbar"><button id="change-date">change&nbsp;date</button><button id="delete-entry">delete</button><button id="hide-entry">hide</button></div>`;
         outputContainer.appendChild(itemElement);
       }
     });
@@ -157,5 +162,8 @@ function renderItems() {
   }
   inputField.focus();
 }
+
+
+
 renderItems();
 
