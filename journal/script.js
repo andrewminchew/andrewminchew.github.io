665bbd5d506951496e76283aaf1bd71e1953c6f5
andrewminchew <andrewminchew@gmail.com>
2023-04-13 21:43:19 -0500

    introduced hide button, delete button, change date button

--- journal/script.js
+++ journal/script.js
@@ -11,15 +11,26 @@ const openSettingsBtn = document.getElementById('open-settings');
 const showHideBtn = document.getElementById('showHideBtn');
 const deleteEntryBtn = document.getElementById('delete-entry');
 const hideEntryBtn = document.getElementById('hide-entry');
+const showAllBtn = document.getElementById('show-all-entries');
+const changeDateBtn = document.getElementById('change-date');
 
-showHideBtn.addEventListener('click', function() {
-    if (document.getElementById('output-container').style.visibility === 'hidden') {
-      document.getElementById('output-container').style.visibility = 'visible';
-      showHideBtn.innerHTML = '<img src="img/hide.svg">';
-    } else {
-      document.getElementById('output-container').style.visibility = 'hidden';
-      showHideBtn.innerHTML = '<img src="img/show.svg">';
-    }
+showAllBtn.addEventListener('click', () => {
+  let itemsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
+  itemsArray.forEach((item) => {
+    item.visibility = 'show';
+  });
+  localStorage.setItem('items', JSON.stringify(itemsArray));
+  renderItems();
+});
+
+showHideBtn.addEventListener('click', function () {
+  if (document.getElementById('output-container').style.visibility === 'hidden') {
+    document.getElementById('output-container').style.visibility = 'visible';
+    showHideBtn.innerHTML = '<img src="img/hide.svg">';
+  } else {
+    document.getElementById('output-container').style.visibility = 'hidden';
+    showHideBtn.innerHTML = '<img src="img/show.svg">';
+  }
 });
 
 reloadBtn.addEventListener('click', () => {
@@ -63,7 +74,7 @@ inputField.addEventListener('keydown', (event) => {
 function submitForm() {
   const inputValue = inputField.value;
   if (inputValue) {
-    const timestamp = new Date().toLocaleString();
+    const timestamp = new Date().toISOString();
     let itemsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
     itemsArray.push({ text: inputValue, timestamp, visibility: "show" });
     localStorage.setItem('items', JSON.stringify(itemsArray));
@@ -71,6 +82,60 @@ function submitForm() {
     renderItems();
   }
 }
+
+outputContainer.addEventListener('click', (event) => {
+  // Use event delegation to listen for clicks on the delete button
+  if (event.target.id === 'delete-entry') {
+    // Get the timestamp of the parent element of the clicked delete button
+    const timestamp = event.target.closest('.entry').querySelector('.timestamp').textContent;
+    let itemsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
+    // Remove the entry with the matching timestamp from the items array
+    itemsArray = itemsArray.filter(item => item.timestamp !== timestamp);
+    localStorage.setItem('items', JSON.stringify(itemsArray));
+    renderItems(); // Refresh the view
+  }
+
+  // Use event delegation to listen for clicks on the hide button
+  if (event.target.id === 'hide-entry') {
+    // Get the timestamp of the parent element of the clicked hide button
+    const timestamp = event.target.closest('.entry').querySelector('.timestamp').textContent;
+    let itemsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
+    // Set the visibility of the entry with the matching timestamp to "hide"
+    itemsArray.forEach(function (item) {
+      if (item.timestamp === timestamp) {
+        item.visibility = "hide";
+      }
+    });
+    localStorage.setItem('items', JSON.stringify(itemsArray));
+    renderItems(); // Refresh the view
+  }
+
+  if (event.target.id === 'change-date') {
+    // Get the timestamp of the parent element of the clicked date change button
+    const timestamp = event.target.closest('.entry').querySelector('.timestamp').textContent;
+    // Show the date picker modal
+    const modal = document.getElementById('date-picker-modal');
+    modal.style.display = "block";
+    const newDatePicker = document.getElementById('new-date-picker');
+    newDatePicker.value = timestamp.split('T')[0]; // Set the initial value of the date picker to the current timestamp
+    // Add an event listener to the "Save" button
+    const saveBtn = document.getElementById('save-date-btn');
+    saveBtn.addEventListener('click', () => {
+      const newDate = new Date(newDatePicker.valueAsNumber);
+      const newTimestamp = newDate.toISOString();
+      let itemsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
+      // Update the timestamp of the entry with the matching timestamp
+      itemsArray.forEach(function (item) {
+        if (item.timestamp === timestamp) {
+          item.timestamp = newTimestamp;
+        }
+      });
+      localStorage.setItem('items', JSON.stringify(itemsArray));
+      renderItems(); // Refresh the view
+      modal.style.display = "none"; // Hide the modal
+    });
+  }
+});
 
 function renderItems() {
   let itemsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
@@ -81,7 +146,7 @@ function renderItems() {
         const itemElement = document.createElement('div');
         itemElement.classList.add('entry');
         itemElement.setAttribute('tabindex', '-1');
-        itemElement.innerHTML = `<div class="text"><span class="timestamp">${item.timestamp}</span>${item.text}</div><div class="toolbar"><button id="delete-entry">delete</button><button id="hide-entry">hide</button>
+        itemElement.innerHTML = `<div class="text"><span class="timestamp">${item.timestamp}</span>${item.text}</div><div class="toolbar"><button id="change-date">change&nbsp;date</button><button id="delete-entry">delete</button><button id="hide-entry">hide</button>
         </div>`;
         outputContainer.appendChild(itemElement);
       }
@@ -92,6 +157,5 @@ function renderItems() {
   }
   inputField.focus();
 }
-
 renderItems();
 
